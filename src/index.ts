457c314ffd3efb85c49ca4975f0d62c938f56c43
Abhinav <143390088+Abhinav-scrrum@users.noreply.github.com>
2023-09-04 16:25:55 +0530

    Fixed the code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 export class log {
     static success(msg: string): void {
-        console.log(`%c ${msg},'color:green'`);
+        console.log(`%c ${msg}`, 'color:green');
 
     }
 
     static danger(msg: string): void {
-        console.log(`%c ${msg},'color:red'`);
+        console.log(`%c ${msg}`, 'color:red');
 
     }
 
     static highlight(msg: string): void {
-        console.log(`%c ${msg},'color:red;background:yellow'`);
+        console.log(`%c ${msg}`, 'color:red;background:yellow');
 
     }
 }
